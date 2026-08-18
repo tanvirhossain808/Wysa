466cd9b481691443b2ed19f07a6978d4ce8c0c4d
John Doe <tanvirhossan528@gmail.com>
2026-08-19 01:35:03 +0600

    feat: serve frontend in production with static file handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import messagesRoutes from "./routes/messageRoutes";
 import usersRoutes from "./routes/userRoutes";
 import { clerkMiddleware } from "@clerk/express";
 import { errorHandler } from "./middleware/errorHandler";
+import path from "path";
 const app = express();
 app.use(express.json());
 app.use(clerkMiddleware());
@@ -17,4 +18,13 @@ app.use("api/chats", chatsRoutes);
 app.use("api/messages", messagesRoutes);
 app.use("api/users", usersRoutes);
 app.use(errorHandler);
+
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 export default app;
