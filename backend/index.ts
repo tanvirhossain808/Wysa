98321874526e672847c16c21dad15acc9091f3c0
John Doe <tanvirhossan528@gmail.com>
2026-08-19 00:47:07 +0600

    feat: implement socket server with user authentication and messaging functionalities

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,15 @@
-import v8 from "node:v8";
-
-// Polyfill the missing Bun implementation manually
-if (!v8.startupSnapshot) {
-  (v8 as any).startupSnapshot = {
-    isBuildingSnapshot: () => false,
-  };
-} else if (!v8.startupSnapshot.isBuildingSnapshot) {
-  v8.startupSnapshot.isBuildingSnapshot = () => false;
-}
-
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
-const port = process.env.PORT || 3000;
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
 
+const port = process.env.PORT || 3000;
+const httpServer = createServer(app);
+initializeSocket(httpServer);
 connectDB()
-  .then(() => app.listen(port, () => console.log("server is running", port)))
+  .then(() =>
+    httpServer.listen(port, () => console.log("server is running", port)),
+  )
   .catch((error) => {
     console.error("failed to start the server", error);
     process.exit(1);
