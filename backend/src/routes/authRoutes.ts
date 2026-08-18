166719120eab52f316f4ad24aaf38cc564bae706
John Doe <tanvirhossan528@gmail.com>
2026-08-18 19:20:18 +0600

    feat: integrate Clerk for authentication and add error handling middleware

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { authCallback, getMe } from "../controllers/authController";
+import { protectRoute } from "../middleware.ts/auth";
 const router = Router();
+
+router.get("/me", protectRoute, getMe);
+router.post("/callback", authCallback);
 export default router;
