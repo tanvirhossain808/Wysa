49363c193ab417d65cc84d784355c2858fa403d7
John Doe <tanvirhossan528@gmail.com>
2026-08-19 23:59:32 +0600

    feat: integrate Sentry for error tracking and user synchronization
    - Updated metro.config.js to use Sentry's Expo configuration.
    - Added @sentry/react-native dependency in package.json.
    - Implemented user synchronization with backend in AuthSync component.
    - Created useAuth hook for handling authentication callbacks and current user queries.
    - Added axios setup with error logging to Sentry.
    - Defined User and Chat types in a new types file.
    - Enhanced RootLayout to initialize Sentry with session replay and logging.

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 export default function index() {
   return (
     <SafeAreaView className="bg-surface text-white flex-1">
       <ScrollView className="flex-1">
         <Text className="text-white">Chats</Text>
+        {/*     <Button
+          title="Try!"
+          onPress={() => {
+            Sentry.captureException(new Error("First error"));
+          }}
+        /> */}
       </ScrollView>
     </SafeAreaView>
   );
--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -3,17 +3,45 @@ import "../../global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { ClerkProvider } from "@clerk/expo";
-
+import AuthSync from "@/components/AuthSync";
+import { StatusBar } from "expo-status-bar";
+import * as Sentry from "@sentry/react-native";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 const queryClient = new QueryClient();
-export default function RootLayout() {
-  if (!publishableKey) {
-    throw new Error("Add your Clerk Publishable Key to the .env file");
-  }
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+Sentry.init({
+  dsn: "https://48d595bfc998dd24b52e5a301e56b8a2@o4511938586279936.ingest.de.sentry.io/4511938599452752",
 
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.reactNativeTracingIntegration({
+      traceFetch: true,
+      traceXHR: true,
+      enableHTTPTimings: true,
+    }),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+export default Sentry.wrap(function RootLayout() {
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <QueryClientProvider client={queryClient}>
+        <AuthSync />
+        <StatusBar style="light" />
         <Stack
           screenOptions={{
             headerShown: false,
@@ -26,4 +54,4 @@ export default function RootLayout() {
       </QueryClientProvider>
     </ClerkProvider>
   );
-}
+});
