f3285d3ac2dd6721cf758beaa73886b34da3ece3
John Doe <tanvirhossan528@gmail.com>
2026-08-21 01:21:03 +0600

    feat: integrate socket.io for real-time messaging and add zustand for state management
    - Added socket.io-client and zustand dependencies.
    - Implemented SocketConnection component to manage socket connection and disconnection based on user authentication.
    - Created useMessages hook to fetch messages for a specific chat.
    - Developed socket store using zustand to handle socket events, online users, typing indicators, and unread messages.
    - Updated ChatItem and MessageBubble components to reflect real-time message updates and user statuses.
    - Enhanced chat functionality in NewChatScreen and ChatDetailScreen to support real-time messaging features.

--- mobile/src/components/ChatItem.tsx
+++ mobile/src/components/ChatItem.tsx
@@ -2,20 +2,17 @@ import { Chat } from "@/types";
 import { Image } from "expo-image";
 import { View, Text, Pressable } from "react-native";
 import { formatDistanceToNow } from "date-fns";
+import { useSocketStore } from "@/lib/socket";
 // import { useSocketStore } from "@/lib/socket";
 
 const ChatItem = ({ chat, onPress }: { chat: Chat; onPress: () => void }) => {
   const participant = chat.participant;
 
-  //   const { onlineUsers, typingUsers, unreadChats } = useSocketStore();
-  // return (
-  //   <View>
-  //     <Text>chat item</Text>
-  //   </View>
-  // );
-  const isOnline = true;
-  const isTyping = false;
-  const hasUnread = true;
+  const { onlineUsers, typingUsers, unreadChats } = useSocketStore();
+
+  const isOnline = onlineUsers.has(participant._id);
+  const isTyping = typingUsers.get(chat._id) === participant._id;
+  const hasUnread = unreadChats.has(chat._id);
 
   return (
     <Pressable
