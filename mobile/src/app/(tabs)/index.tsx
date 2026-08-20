6ab13cf6379a143e4323942500df0eaa55564c52
John Doe <tanvirhossan528@gmail.com>
2026-08-20 12:19:45 +0600

    feat: add chat functionality with chat list and detail views

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,17 +1,99 @@
-import { ScrollView, Text } from "react-native";
+import ChatItem from "@/components/ChatItem";
+import EmptyUI from "@/components/EmptyUi";
+import { useChats } from "@/hooks/useChats";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-export default function index() {
+export default function ChatsTab() {
+  const router = useRouter();
+  const { data: chats, isLoading, error } = useChats();
+
+  if (isLoading)
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <ActivityIndicator size="large" color="#f4A261" />
+      </View>
+    );
+  if (error) {
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <Text className="text-red-500 text-3xl">Failed to load chats</Text>
+        <Pressable
+          // onPress={() => refetch()}
+          className="mt-4 px-4 py-2 bg-primary rounded-lg"
+        >
+          <Text className="text-foreground">Retry</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+  const handleChatPress = (chat: Chat) => {
+    router.push({
+      pathname: "/chat/[id]",
+      params: {
+        id: chat._id,
+        participantId: chat.participant._id,
+        name: chat.participant.name,
+        avatar: chat.participant.avatar,
+      },
+    });
+  };
+
   return (
-    <SafeAreaView className="bg-surface text-white flex-1">
-      <ScrollView className="flex-1">
-        <Text className="text-white">Chats</Text>
-        {/*     <Button
-          title="Try!"
-          onPress={() => {
-            Sentry.captureException(new Error("First error"));
-          }}
-        /> */}
-      </ScrollView>
+    <SafeAreaView className="flex-1 bg-surface">
+      <FlatList
+        data={chats}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <ChatItem chat={item} onPress={() => handleChatPress(item)} />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{
+          paddingHorizontal: 20,
+          paddingTop: 16,
+          paddingBottom: 24,
+        }}
+        ListHeaderComponent={<Header />}
+        ListEmptyComponent={
+          <EmptyUI
+            title="No chats yet"
+            subtitle="Start a conversation!"
+            iconName="chatbubbles-outline"
+            iconColor="#6B6B70"
+            iconSize={64}
+            buttonLabel="New Chat"
+            // onPressButton={() => router.push("/new-chat")}
+          />
+        }
+      />
     </SafeAreaView>
+  );
+}
+
+function Header() {
+  const router = useRouter();
+
+  return (
+    <View className="px-5 pt-2 pb-4">
+      <View className="flex-row items-center justify-between">
+        <Text className="text-2xl font-bold text-foreground">Chats</Text>
+        <Pressable
+          className="size-10 bg-primary rounded-full items-center justify-center"
+          // onPress={() => router.push("/new-chat")}
+        >
+          <Ionicons name="create-outline" size={20} color="#0D0D0F" />
+        </Pressable>
+      </View>
+    </View>
   );
 }
